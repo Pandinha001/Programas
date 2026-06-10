5327c1e1b9b7d52d8d96e012da1dc7b4dd0a6dce
Aline <aline09ribeirao@gmail.com>
2026-06-10 16:42:31 -0300

    Update and rename PO to quest1.py

---
+++ quest1.py
@@ -0,0 +1,24 @@
+class Carteira:
+  def __init__(self, moeda: str, saldo: float):
+    self.moeda = moeda
+    self.saldo = saldo
+  
+  def converter(self, val_yuan):
+    if self.moeda == 'USD':
+      return self.saldo*0.14:
+    elif self.moeda == 'BRL':
+      return self.saldo*0.85
+  def __add__(self, val_yuan):
+    if self.moeda != 'YUAN':
+      return self.saldo + converter(val_yuan)
+    else:
+      return self.saldo + val_yuan
+  def __sub__(self, val_yuan):
+    if self.moeda != 'YUAN':
+      return self.saldo - converter(val_yuan)
+    else:
+      return self.saldo - val_yuan
+
+
+carteira_usd = Carteira("USD", 10.0)
+print('Soma de carteira USD + 50 yuan = ', carteira_usd + 50.0)
