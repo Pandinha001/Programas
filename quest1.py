3643aa5d48ba822bbe297d3e7ee619097d034f67
Aline <aline09ribeirao@gmail.com>
2026-06-10 16:53:27 -0300

    Update2 quest1.py

--- quest1.py
+++ quest1.py
@@ -5,20 +5,23 @@ class Carteira:
   
   def converter(self, val_yuan):
     if self.moeda == 'USD':
-      return self.saldo*0.14:
+      return self.saldo*0.14
     elif self.moeda == 'BRL':
       return self.saldo*0.85
+    else:
+      return val_yuan
   def __add__(self, val_yuan):
     if self.moeda != 'YUAN':
-      return self.saldo + converter(val_yuan)
+      return self.saldo + self.converter(val_yuan)
     else:
       return self.saldo + val_yuan
   def __sub__(self, val_yuan):
     if self.moeda != 'YUAN':
-      return self.saldo - converter(val_yuan)
+      return self.saldo - self.converter(val_yuan)
     else:
       return self.saldo - val_yuan
 
-
 carteira_usd = Carteira("USD", 10.0)
 print('Soma de carteira USD + 50 yuan = ', carteira_usd + 50.0)
+carteira_brl = Carteira("BRL", 30.0)
+print('Soma de carteira BRL + 20 yuan = ', carteira_brl + 20.0)
