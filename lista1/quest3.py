a0d5fc7a0b027e4301a2536386e1abe6ad96a44f
Aline <aline09ribeirao@gmail.com>
2026-06-15 10:48:14 -0300

    Create quest3.py

---
+++ lista1/quest3.py
@@ -0,0 +1,14 @@
+class Onibus:
+  def __init__(self, placa: str, motorista: str, num_assentos: int):
+    self.placa = placa
+    self.motorista = motorista
+    self.assentos = num_assentos
+    
+  def __len__(self):
+    pass
+  def __getitem__(self, indice):
+    pass
+  def __setitem__(self, indice, valor):
+    pass
+  def __str__(self):
+    pass
