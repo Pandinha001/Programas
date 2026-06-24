7fe71736692117168a9d81a94991350d1cec666f
Aline <aline09ribeirao@gmail.com>
2026-06-24 16:41:57 -0300

    Update quest3.py

--- lista1/quest3.py
+++ lista1/quest3.py
@@ -2,13 +2,31 @@ class Onibus:
   def __init__(self, placa: str, motorista: str, num_assentos: int):
     self.placa = placa
     self.motorista = motorista
-    self.assentos = num_assentos
+    self.assentos = [False]*num_assentos
     
   def __len__(self):
-    pass
+      return len(self.assentos)
   def __getitem__(self, indice):
-    pass
+    if indice > len(self.assentos) or indice < 0:
+            raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
+     else:
+            return self.assentos[indice]  
   def __setitem__(self, indice, valor):
-    pass
+   if indice > len(self.assentos) or indice < 0:
+            raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
+        else:
+            if not isinstance(valor, bool):
+                raise TypeError(f"Valor deve ser booleano (True/False)")
+            else:
+                self.assentos[indice] = valor
   def __str__(self):
-    pass
+    return f'Assentos: {self.assentos}'
+
+onibus = Onibus(231, 'Carlos Eduardo', 10)
+
+print(len(onibus))
+
+onibus[0] = True
+onibus[1] = True
+
+print(onibus)
