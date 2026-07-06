13fbe7550fdcfec2c657594c02547326aa06ff3a
Aline <aline09ribeirao@gmail.com>
2026-07-06 20:43:13 -0300

    Update strings.py
    questões 4 e 5

--- lista2/strings.py
+++ lista2/strings.py
@@ -1,10 +1,10 @@
-#questão 01
+#questão 1 lab9
 
 nome = input("Digite seu primeiro nome: ").strip()
 sobrenome = input("Digite seu sobrenome: ").strip()
 print(f"Bem-vinda, {nome} {sobrenome}!")
 
-#questão 02
+#questão 2
 
 frase = input("Digite a frase: ")
 cont = 0
@@ -13,10 +13,34 @@ for caractere in frase:
         cont = cont + 1
 print("Espaços em branco:", cont)
 
-#questão 03
+#questão 3
 
 nome = input("Digite seu nome: ")
 escada = ""
 for letra in nome:
     escada = escada + letra
     print(escada)
+
+
+#questão 4
+num = input("Número: ")
+if len(num) == 9:
+    if num[0] == "9":  
+        print(f"Número Completo: {num[:5]}-{num[5:]}")
+    else:
+        print("Número Incorreto!")
+elif len(num) == 8:
+    print(f"Número Completo: 9{num[:4]}-{num[4:]}")
+else:
+    print("Número Incorreto!")
+
+#questão 5
+txt = input("Frase: ")
+ind = []
+qnt = 0
+for i, letra in enumerate(txt):
+    if letra in "AEIOUaeiou":
+        ind.append(i)
+        qnt += 1
+print(f"Índices: {", ".join(ind)}")
+print("Total:",qnt)
